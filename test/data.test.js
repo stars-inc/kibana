8c6904baacd5b0eddeb27570b53579a0f2811161
Alex <stars_inc@mail.ru>
2023-04-07 21:23:50 +0300

    feat: add comments

--- test/data.test.js
+++ test/data.test.js
@@ -3,26 +3,30 @@ const request = require('supertest')
 describe("recived data", () => {
   const { express, data } = require('../app/app')
 
-  let body
-  const objectKeys = Object.keys(data._source)
+  let body // declared varible to get them into lifecirle hook
+  const objectKeys = Object.keys(data._source) // get keys _source object from data.json file as axpected keys
 
-  beforeEach(async () => {
+  beforeEach(async () => { // lifecircle hook generated new response before each test
     body = (await request(express).get("/")).body
   })
 
+  // test:respose.body is not falsy ( null, undefined )
   it("sould not be falsy", () => {
     expect(body).not.toBeFalsy()
   })
 
+  // test:respose.body is not empty object {} && has min one key
   it("sould be length", () => {
     const keys = Object.keys(body)
     expect(keys.length).toBeGreaterThanOrEqual(1)
   })
 
+  // test:response.body to check mutated data before respose.body must be strict equal data
   it("sould contain strict equal data", () => {
     expect(body).toEqual(data)
   })
 
+  // test:response.body._source to contain all keys from moked data object
   it("sould contain strict equal keys", () => {
     Object.keys(body._source).forEach((item, i) => 
       expect(item).toEqual(objectKeys[i]) 
